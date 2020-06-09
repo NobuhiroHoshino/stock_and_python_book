48f333c90452919bb9d370d405c0247402f4dcac
Nobuhiro Hoshino <e-dolphin@sannet.ne.jp>
2020-06-09 16:19:57 +0900

    20200609 get_brands.py動作成功！

--- chapter2/get_brands.py
+++ chapter2/get_brands.py
@@ -4,6 +4,8 @@ import time
 import sqlite3
 
 
+
+#銘柄情報の取得
 def get_brand(code):
   url = 'https://kabutan.jp/stock/?code={}'.format(code)
 
@@ -13,6 +15,8 @@ def get_brand(code):
     return None
 
   try:
+    #CSSセレクタで、直接HTMLの要素を指定していると思われる。
+    #元の画面構成が変わったら変更しなければいけないんだろうね
     name = q.find('div.company_block > h3').text()
     code_short_name =  q.find('#stockinfo_i1 > div.si_i1_1 > h2').text()
     short_name = code_short_name[code_short_name.find(" ") + 1:]
@@ -25,6 +29,7 @@ def get_brand(code):
 
   return code, name, short_name, market, unit, sector
 
+#code_rangeに入れたリストで、上記の銘柄情報取得のループを回す。
 def brands_generator(code_range):
   for code in code_range:
     brand = get_brand(code)
@@ -32,9 +37,18 @@ def brands_generator(code_range):
       yield brand
     time.sleep(1)
 
+#SQLiteに書き込みするが、結局code_rangeはどこかで確保する必要がある。
+#SQLiteのデータベースも確保しておく必要がある。
 def insert_brands_to_db(db_file_name, code_range):
   conn = sqlite3.connect(db_file_name)
   with conn:
-    sql = 'INSERT INTO brands(code,name,short_name,market,unit,sector) ' \
+    sql = 'INSERT INTO brands(code,name,shortname,market,unit,sector) ' \
           'VALUES(?,?,?,?,?,?)'
     conn.executemany(sql, brands_generator(code_range))
+
+#mainはなかったので付け足した。
+def main():
+  db = "C:\\Users\\Nobuhiro Hoshino\\PycharmProjects\\stock_and_python_book\\chapter2\\StockPrices.db"
+  insert_brands_to_db(db, range(1301,9999))
+
+main()
