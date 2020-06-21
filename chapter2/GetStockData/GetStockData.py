492febf0e37ce8fa1ba99c98749a57103557b5b0
Nobuhiro Hoshino <e-dolphin@sannet.ne.jp>
2020-06-21 16:21:18 +0900

    20200621 分割取得モデル完成

--- chapter2/GetStockData/GetStockData.py
+++ chapter2/GetStockData/GetStockData.py
@@ -20,14 +20,16 @@
 
 from bs4 import BeautifulSoup
 import pandas as pd
+# import pandas.io.sql 記述しなくてもいいらしい
 import requests
 from datetime import datetime
 import time
 import sqlite3
 
-def get_dfs(stock_number):
+def get_dfs(stock_number, year):
+#stocknumberはコード番号
+#yearは必要な年のリスト
     dfs = []
-    year = range(2018,2020)
     for y in year:
         try:
             print(y)
@@ -55,6 +57,7 @@ def get_dfs(stock_number):
             time.sleep(2)
         except IndexError:
             print('No data',stock_number,y)
+            time.sleep(1)
     return dfs
 
 def concatenate(dfs):
@@ -72,27 +75,56 @@ def get_price_dataframe(db_file_name):
     return pd.read_sql('SELECT code, name FROM brands ORDER BY code',conn)
 
 def AMain():
-    db='C:\\Users\\Nobuhiro Hoshino\\PycharmProjects\\stock_and_python_book\\chapter2\\StockPrices.db'
-    cl=get_price_dataframe(db)
+    SQLFILE=r'C:\Users\Nobuhiro Hoshino\PycharmProjects\stock_and_python_book\chapter2\StockPrices.db'
+    CSVPATH=r'C:\Users\Nobuhiro Hoshino\Documents\stock\CSV'
+
+    cl=get_price_dataframe(SQLFILE)
     code_list=pd.DataFrame(cl,columns=['code', 'name'])
+
+#１日５００銘柄とする。４０５９銘柄委あるので、８日間で終わる
+    listnumber=len(code_list)
+    day1=range(0,500)
+    day2=range(500,1000)
+    day3=range(1000,1500)
+    day4=range(1500,2000)
+    day5=range(2000,2500)
+    day6=range(2500,3000)
+    day7=range(3000,3500)
+    day8=range(3500,listnumber)
+
+    #ここで変数範囲を選択。あとは手動で変える
+    todaylist=day1
 
 #複数のデータフレームをcsvで保存
 #ほっとくとINDEXが入るので、パラメータで指定する
 #ファイルが存在する場合は、上書きされる
+    conn = sqlite3.connect(SQLFILE)
+    cursor = conn.cursor()
+    if todaylist==day1:
+        cursor.execute('DELETE FROM prices') #初回前提なんで、テーブルのデータは全削除
 
-    for i in range(len(code_list)):
+    for i in todaylist:
         k = code_list.loc[i,'code']
         v = code_list.loc[i,'name']
         print(k,v)
-        dfs = get_dfs(k)
+        dfs = get_dfs(k,range(1983,2021))
         data = concatenate(dfs)
 
-        #dataframeのcolを変える必要あり
-        #dataframeをSQLiteに一気に書き出す方法を探す必要あり。
-        
+        #dataframeのcolを変える。renameならdictで変更前、変更後を設定する。ピンポイントで変更が可能。
+        #data.columns=['date','open'・・・]でリストを入れれば、丸ごとの変更が可能。
+        renameindex = {'日付':'date','始値':'open','高値':'high' ,
+                       '安値':'low','終値':'close','出来高':'volume','終値調整':'ajustedclose'}
+        data=data.rename(columns= renameindex) #renameは本体を書き換えないので
 
-        data.to_csv('{}-{}.csv'.format(k,v),header=True,index=False)
+        #code列がないんで追加が必要
+        #順番にこだわらなければ、代入すれば勝手にできる。便利。いろいろやり方はあるが、位置指定できるのは、insert
+        data.insert(1,'code',k)#insertは本体を書き換える。ついでにcodeで埋められる。便利。
 
+        data.to_csv('{}\\{}-{}.csv'.format(CSVPATH, k, v), header=True, index=False)
 
+        #dataframeをSQLiteに一気に書き出す
+        data.to_sql('prices',conn,if_exists='append',index=None)
+        #appendはテーブルがある場合は追加。replaceはそっくりテーブルを書き換えてしまう模様。
+        #また、dataframeはfloatらしいので、格納するとき型がどうなるか不安
 
 AMain()
