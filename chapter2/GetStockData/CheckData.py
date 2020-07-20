772b1a67ff9ab734658173c71833dbf5e3725a82
Nobuhiro Hoshino <e-dolphin@sannet.ne.jp>
2020-07-20 22:00:36 +0900

    20200720 既存データベースのデータ取得漏れルーチン完成と思ったら、最近の分が抜けていたので改めて追加！

--- chapter2/GetStockData/CheckData.py
+++ chapter2/GetStockData/CheckData.py
@@ -10,14 +10,17 @@ def CheckOneBrand(codes):
     #営業日取得。priceデータから日付情報だけをソートして抽出して重複を削除すれば、営業日情報になる。（さすがに１銘柄も情報のない営業日はないと信じたい）
     sql='SELECT DISTINCT date FROM prices ORDER BY date ASC;'
     dates = pd.read_sql(sql, conn)
-    dates.to_csv('businessday.csv')
+    # dates.to_csv('businessday.csv')
+    # dates = pd.read_csv('businessday.csv')
+
+    cols = ['date', 'code']
+    dfNoData = pd.DataFrame(index=[], columns=cols) #データがない日付と銘柄保存用の空DF
 
     for code in codes:
         sql='SELECT * FROM prices WHERE code="' + code +'" ORDER BY date ASC;'
         brandData = pd.read_sql(sql, conn)
 
-        #直近データが取れていないことにこれは気づけない。
-        # brandsデータの日付でループしているので。。
+        # ０列はdate列である前提。
         oldestDate = brandData.iloc[0, 0]
         la = len(dates.query('date >= "' + oldestDate + '"'))
         lb = len(brandData.query('date >= "' + oldestDate + '"'))
@@ -29,17 +32,23 @@ def CheckOneBrand(codes):
             j = 0
             for i in range(lb):
                 if dates.iloc[j, 0] != brandData.iloc[i, 0]:
-                    print(dates.iloc[j, 0], code)
+                    record = pd.Series([dates.iloc[j, 0], code], index=dfNoData.columns)    #seriesにしてからDFに追加
+                    dfNoData = dfNoData.append(record, ignore_index=True)   #index振り直し
+
                     while dates.iloc[j, 0] != brandData.iloc[i, 0] and j <= la:
                         j += 1
                 j += 1
 
-        #     これだと、brandsデータが最新でないと、直近取り漏れがあると判定できない。
+            newestDate = brandData.iloc[-1, 0]
+            noDataDates = dates.query('date > "' + newestDate + '"').copy() # copyをつけないとスライスか別dfかわからんのでワーニング
+            noDataDates['code'] = code
+            dfNoData = dfNoData.append(noDataDates, ignore_index = True)
 
         else:
             print(code, 'ok')
 
     conn.close()
+    print(dfNoData)
 
 def CheckBrands():
     codes=['1301']
